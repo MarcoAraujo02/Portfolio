299d01afa3cc787a46878a1a799367659d817062
marcoaraujo02 <marcoaraujo0w@gmail.com>
2024-10-22 10:42:50 -0300

    Curriculum projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+
+import ecoocean from "../../Assets/Projects/ecoocean.jpg"
+import triagem from"../../Assets/Projects/triagem.jpg"
+import save from "../../Assets/Projects/saveourocean.jpg"
+import screen from "../../Assets/Projects/screensound.jpg"
 
 function Projects() {
   return (
@@ -23,48 +22,48 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={ecoocean}
               isBlog={false}
               title="EcoOcean"
               description="O projeto é um sistema de gerenciamento de eventos que atende tanto administradores quanto voluntários. Os usuários iniciam o acesso por meio de um login, onde os administradores têm acesso manual, enquanto os voluntários fazem login para visualizar eventos disponíveis.        
               User - Caio -
               Senha - 123"
               ghLink="https://github.com/MarcoAraujo02/EcoOcean"
-              demoLink="https://chatify-49.web.app/"
+   
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={screen}
               isBlog={false}
               title="ScreenSound"
               description="ScreenSound é API criada para gerencimanto de um sistema de musicas. podendo excluir, editar e criar artistas para o App, relacionando artistas a seus devidos albuns e musics. A Api se conecta com o front-end feito com Blazor web"
               ghLink="https://github.com/MarcoAraujo02/ScreenSound.git"
-              demoLink="https://blogs.soumya-jit.tech/"
+
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={triagem}
               isBlog={false}
               title="InScreening"
               description="InScreening é uma API criada para sistemas de hospitais e triagem, utilizando uma inteligencia artificial criada com python e treinada com tensorflow
                para fazer os exames de forma rapida e precisa."
               ghLink="https://github.com/MarcoAraujo02/InScreeningApi.git"
-              demoLink="https://editor.soumya-jit.tech/"              
+             
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={save}
               isBlog={false}
               title="Save Our Ocean"
               description="O aplicativo permite que Voluntários cadastrem áreas com poluição, informando CEP, tipo de poluição e descrição. Eles também podem verificar o status das áreas, especialmente as marcadas como Limpa. O Administrador gerencia voluntários e atualiza o status das áreas para Limpa, mostrando o progresso das ações de limpeza."
               ghLink="https://github.com/MarcoAraujo02/Save_Our_Ocean.git"
-              demoLink="https://plant49-ai.herokuapp.com/"
+
             />
           </Col>
 
